ffddc02c9cffd1bd9f82fa35044078423de2016e
Kottalo <kottaloflotenspieler@gmail.com>
2026-02-13 14:40:24 +0800

    Checkpoint

--- autoloads/game/game.gd
+++ autoloads/game/game.gd
@@ -13,6 +13,9 @@ extends Node
 @export var setting_page: SettingPage
 @export var loading_page: LoadingPage
 @export var sv_container: SubViewportContainer
+@export var bgm_player: AudioStreamPlayer
+
+@onready var eq: AudioEffectEQ = AudioServer.get_bus_effect(0, 0)
 
 var loading: bool:
 	set(value):
@@ -24,22 +27,30 @@ func _ready() -> void:
 	hide_all_pages()
 	main_menu.show()
 	
-	bonus_page.music_page.audio_player.play()
-	
-	stage_page.visibility_changed.connect(bonus_page.music_page.update_pause)
+	stage_page.visibility_changed.connect(update_pause)
+
+func update_pause():
+	bgm_player.stream_paused = stage_page.visible
+
 
 func hide_all_pages() -> void:
 	for page: CanvasLayer in page_pool.get_children():
 		page.layer = 1
 		page.visible = false
 
+
 func fade(fade_in: bool) -> void:
-	var start_value = 20 if fade_in else 1
-	var end_value = 1 if fade_in else 20
-	
+	var start_iteration = 20 if fade_in else 1
+	var start_db = -60 if fade_in else 0
 	await create_tween().tween_method(
-		func(value): sv_container.material.set_shader_parameter("iterations", value),
-		start_value, end_value, 0.4
+		func(value):
+			var modifier = -1 if fade_in else 1
+			var add = 19 * value * modifier
+			var db_add = 60 * value * -modifier
+			for i in range(2, 4):
+				eq.set_band_gain_db(i, start_db + db_add)
+			sv_container.material.set_shader_parameter("iterations", start_iteration + add),
+		0.0, 1.0, 0.4
 	).finished
 
 func transition(callable: Callable):
