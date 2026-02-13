8404f567482295a60f92a5f34374b113bfc29f5c
Kottalo <kottaloflotenspieler@gmail.com>
2026-02-14 00:53:23 +0800

    Checkpoint

--- autoloads/game/game.gd
+++ autoloads/game/game.gd
@@ -14,9 +14,6 @@ extends Node
 @export var loading_page: LoadingPage
 @export var sv_container: SubViewportContainer
 @export var bgm_player: AudioStreamPlayer
-@export var noise_player: AudioStreamPlayer
-
-@onready var effect_band_filter: AudioEffectBandPassFilter = AudioServer.get_bus_effect(2, 1)
 
 var loading: bool:
 	set(value):
@@ -39,26 +36,16 @@ func hide_all_pages() -> void:
 		page.layer = 1
 		page.visible = false
 
-
 func fade(fade_in: bool) -> void:
-	var start_iteration = 20 if fade_in else 1
-	var start_hz = 20500 if fade_in else 1
-	if not fade_in:
-		noise_player.play()
+	var start_iteration = 1 if fade_in else 0
 	var tween = create_tween()
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	await tween.tween_method(
 		func(value):
 			var modifier = -1 if fade_in else 1
-			var add = 19 * value * modifier
-			var hz_add = 20499 * value * modifier
-			var hz = start_hz + hz_add
-			if not fade_in:
-				effect_band_filter.cutoff_hz = hz + 1
-			sv_container.material.set_shader_parameter("iterations", start_iteration + add),
+			sv_container.material.set_shader_parameter("iterations", start_iteration + (value * modifier)),
 		0.0, 1.0, 0.4
 	).finished
-	noise_player.stop()
 
 func transition(callable: Callable):
 	await fade(false)
