b43b79ff97d70fcbf1664e0a97cc96b27d5333bd
Kottalo <kottaloflotenspieler@gmail.com>
2026-02-27 20:03:28 +0800

    Checkpoint

---
+++ data/_models/expression_data.gd
@@ -0,0 +1,10 @@
+@tool
+class_name ExpressionData
+extends Resource
+
+@export_tool_button("Test") var test = func():
+	print(11)
+@export var name: String
+@export var eyebrows: String
+@export var eyes: String
+@export var mouth: String
