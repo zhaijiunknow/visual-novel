b78c4f1199257c07002f49e7cb9a38c070f1ec17
Kottalo <kottaloflotenspieler@gmail.com>
2026-04-06 23:55:25 +0800

    Checkpoint

--- prefabs/dialogue_button/dialogue_button.gd
+++ prefabs/dialogue_button/dialogue_button.gd
@@ -4,47 +4,63 @@ extends TextureRect
 signal toggle_changed
 signal clicked
 
-@export var hint_text: String
+@export var hint_text: String:
+	set(value):
+		hint_text = value
+		if _hint_label:
+			_hint_label.text = hint_text
+
 @export var toggle_button: bool
 @export var select_frame: TextureRect
 @export var icon: TextureRect
 @export var click_rect: Control
+@export var hint_panel: Control
 
 var color_normal = Color(1.0, 1.0, 1.0)
 var color_hover = Color(0.7, 0.7, 0.7)
 var color_click = Color(0.5, 0.5, 0.5)
-var color_off = Color(0.0, 0.0, 0.0, 0)
+
+var _hint_label: Label
 
 @export var toggled: bool:
 	set(value):
 		toggled = value
-		
-		var frame_on = toggle_button and toggled
-		select_frame.self_modulate = color_normal if frame_on else color_off
-		click_rect.mouse_filter = Control.MOUSE_FILTER_STOP if frame_on \
-			else Control.MOUSE_FILTER_IGNORE
-		
+		if toggle_button:
+			select_frame.self_modulate = color_normal if toggled else Color.TRANSPARENT
 		toggle_changed.emit()
+
+var hovered: bool:
+	set(value):
+		hovered = value
+		hint_panel.visible = hovered
+		modulate = color_hover if hovered else color_normal
 
 var disabled: bool:
 	set(value):
 		disabled = value
 		if disabled:
 			toggled = false
+			hovered = false
 		modulate.a = 0.3 if disabled else 1.0
 
 func _ready() -> void:
-	icon.mouse_entered.connect(
+	_hint_label = hint_panel.find_child("Label", false, false) as Label
+	if _hint_label:
+		_hint_label.text = hint_text
+
+	hint_panel.visible = false
+
+	click_rect.mouse_entered.connect(
 		func ():
 			if disabled: return
-			icon.modulate = color_hover
+			hovered = true
 	)
-	icon.mouse_exited.connect(
+	click_rect.mouse_exited.connect(
 		func ():
 			if disabled: return
-			icon.modulate = color_normal
+			hovered = false
 	)
-	icon.gui_input.connect(
+	click_rect.gui_input.connect(
 		func (event: InputEvent):
 			if disabled: return
 			if event is InputEventMouseButton:
@@ -57,5 +73,5 @@ func _ready() -> void:
 					if event.is_released():
 						icon.modulate = color_normal
 	)
-	
+
 	toggled = toggled
