d1421b5571292df1c07ef6bc846c0518696321dd
Kottalo <kottaloflotenspieler@gmail.com>
2026-04-08 16:04:11 +0800

    Checkpoint

--- scripts/save_data.gd
+++ scripts/save_data.gd
@@ -5,6 +5,8 @@ extends Resource
 @export var auto_profile: ProfileData
 @export var read_data_list: Array[ReadData]
 
+var _save_pending: bool = false
+var _save_thread: Thread
 
 func get_read_data(chapter_name: String) -> ReadData:
 	for rd in read_data_list:
@@ -20,7 +22,20 @@ func mark_read(chapter_name: String, line_id: int) -> void:
 		read_data_list.append(rd)
 	if line_id > rd.last_read_id:
 		rd.last_read_id = line_id
-		ResourceSaver.save(self, "user://save_data.tres")
+		_save_deferred()
+
+func _save_deferred() -> void:
+	if _save_pending:
+		return
+	_save_pending = true
+	if _save_thread and _save_thread.is_started():
+		_save_thread.wait_to_finish()
+	_save_thread = Thread.new()
+	_save_thread.start(
+		func():
+			ResourceSaver.save(self, "user://save_data.tres")
+			_save_pending = false
+	)
 
 func is_line_read(chapter_name: String, line_id: int) -> bool:
 	var rd = get_read_data(chapter_name)
