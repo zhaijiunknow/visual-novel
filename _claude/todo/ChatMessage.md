ba73e3998e65caf232c0932ea3e09b5ccb33faca
Kottalo <kottaloflotenspieler@gmail.com>
2026-04-09 21:40:29 +0800

    Checkpoint

---
+++ _claude/todo/ChatMessage.md
@@ -0,0 +1,7 @@
+如果超过或等于父级宽度的话：
+frame_right.modulate.a = 1
+frame_left.modulate.a = 0
+以此类推啦
+
+不要用隐藏
+
--- prefabs/chat_message/chat_message.gd
+++ prefabs/chat_message/chat_message.gd
@@ -1,11 +1,10 @@
-@tool
 class_name ChatMessage
 extends HBoxContainer
 
 @export var sender_type: Enums.SenderType:
 	set(value):
 		sender_type = value
-		update.call_deferred()
+		update()
 
 @export var self_bubble_color: Color
 @export var frame_left: Control
@@ -19,9 +18,8 @@ extends HBoxContainer
 var parent: Control:
 	get: return get_parent()
 
-#func _ready() -> void:
-	##if Engine.is_editor_hint(): return
-	#resized.connect(update)
+func _ready() -> void:
+	resized.connect(update)
 
 func setup(type: Enums.SenderType, text: String, avatar: Texture2D = null) -> void:
 	sender_type = type
@@ -29,26 +27,35 @@ func setup(type: Enums.SenderType, text: String, avatar: Texture2D = null) -> vo
 	if avatar:
 		avatar_left.texture = avatar
 		avatar_right.texture = avatar
-	bubble.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	message_text.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+	update.call_deferred()
 
 func update() -> void:
 	var is_self = sender_type == Enums.SenderType.SELF
-	#size_flags_horizontal = SIZE_EXPAND_FILL
-	frame_left.modulate.a = 1 if not is_self else 0
-	frame_right.modulate.a = 1 if is_self else 0
+
+	# 头像：自己显示右边，对方显示左边
+	avatar_left.visible = not is_self
+	avatar_right.visible = is_self
+
+	# 气泡颜色
 	message_text.self_modulate = Color.WHITE if is_self else Color.BLACK
 	bubble.self_modulate = self_bubble_color if is_self else Color.WHITE
 
-	#if is_self:
-		#message_text.alignment = BoxContainer.ALIGNMENT_END
-	#else:
-		#message_text.alignment = BoxContainer.ALIGNMENT_BEGIN
-
-	if  size.x >= parent.size.x:
-		bubble_margin.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	# 先用不换行测量文字自然宽度
+	if size.x >= parent.size.x:
+		size_flags_horizontal = Control.SIZE_EXPAND_FILL
 		message_text.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
 	else:
-		bubble_margin.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+		size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
 		message_text.autowrap_mode = TextServer.AUTOWRAP_OFF
-	message_text.size = Vector2()
+
+	var pool_width = get_parent().size.x if get_parent() else size.x
+	if size.x >= pool_width:
+		# 文字太长，需要换行填满
+		bubble_margin.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		message_text.autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+
+	# 对齐：自己靠右，对方靠左
+	if is_self:
+		alignment = BoxContainer.ALIGNMENT_END
+	else:
+		alignment = BoxContainer.ALIGNMENT_BEGIN
